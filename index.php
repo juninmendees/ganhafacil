72f188e0a4c0319db6a7586fbf0032f7c57e3f89
Junior Mendes <juniormendesjp@gmail.com>
2019-05-19 18:08:17 -0300

    criando estruturas iniciais

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 
+$blocoEspecial= array('05', '06','07','12','13','14','19','20','21');
+$blocoGeral = array ('01','02','03','04','08','09','10','11','15','16','17','18','22','23','24','25');
 
+$quantidadeNumerosBilhete = 15;
+
+$padraoApostaDefaul = 5;
+for ($i=1; $i <$padraoApostaDefaul ; $i++) { 
+		
+}
+
+/* Criar função Sortear, que deve popular o array $resultado e imprimir na tela.
+Essa função deve gerar a quantidade de numero informada na variavel $padraoApostaDefault
+E completar com a quantidade de numeros do array $blocogeral até um total de 15 numeros
+
+*/
 ?>
+
